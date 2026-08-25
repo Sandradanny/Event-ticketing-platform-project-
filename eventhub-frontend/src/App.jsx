3db8c5141033208367f1baad08f8e999d9ea01a1
Sandradanny <sandradan06@gmail.com>
2026-08-25 10:50:33 +0100

    Add homepage components and project structure

--- eventhub-frontend/src/App.jsx
+++ eventhub-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import EventDetail from "./pages/EventDetail";
 import Login from "./pages/Login";
@@ -7,6 +8,7 @@ import Signup from "./pages/Signup";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/events" element={<SearchResults />} />
       <Route path="/event/:id" element={<EventDetail />} />
       <Route path="/login" element={<Login />} />
